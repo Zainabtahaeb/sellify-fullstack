dac7101727e0dfe05e20fa72eb91ebec2a3466fd
Zainab Taha <zainabtaha993@gmail.com>
2026-04-08 21:28:26 +0300

    fix cors final

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
+app.options('*', cors());
 app.use(express.json());
 
 // test route
